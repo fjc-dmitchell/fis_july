56a1adb50a83fc1c4bdaf63d018e894b2f6ebb7b
Doug Mitchell <dmitchell@fjc.gov>
2024-03-04 14:02:28 -0500

    Initial commit of FIS 2.1 with Jmix 2.2.0

---
+++ src/main/java/gov/fjc/fis/view/user/UserListView.java
@@ -0,0 +1,14 @@
+package gov.fjc.fis.view.user;
+
+import gov.fjc.fis.entity.User;
+import gov.fjc.fis.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "users", layout = MainView.class)
+@ViewController("fis_User.list")
+@ViewDescriptor("user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+public class UserListView extends StandardListView<User> {
+}
