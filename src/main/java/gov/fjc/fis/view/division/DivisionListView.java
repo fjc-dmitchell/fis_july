34258713b4ac5b9cd21fdd52fc45a919501a329b
Doug Mitchell <dmitchell@fjc.gov>
2024-09-19 13:16:31 -0400

    september 19

--- src/main/java/gov/fjc/fis/view/division/DivisionListView.java
+++ src/main/java/gov/fjc/fis/view/division/DivisionListView.java
@@ -1,24 +1,12 @@
 package gov.fjc.fis.view.division;
 
-import gov.fjc.fis.entity.Appropriation;
 import gov.fjc.fis.entity.Division;
 
-import gov.fjc.fis.service.DivisionService;
 import gov.fjc.fis.view.main.MainView;
 
 import com.vaadin.flow.router.Route;
-import gov.fjc.fis.view.quicksearchcomponent.QuickSearchComponent;
 import gov.fjc.fis.view.search.CustomSearchFragment;
-import io.jmix.core.LoadContext;
-import io.jmix.core.session.SessionData;
-import io.jmix.flowui.UiComponents;
-import io.jmix.flowui.model.CollectionContainer;
-import io.jmix.flowui.model.CollectionLoader;
 import io.jmix.flowui.view.*;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Route(value = "divisions", layout = MainView.class)
 @ViewController("fis_Division.list")
