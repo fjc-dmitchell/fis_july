2409fb818f927021703b239c5ce5bc2d1585df40
Doug Mitchell <dmitchell@fjc.gov>
2025-03-25 14:25:30 -0400

    vendor status checkbox

--- src/main/java/gov/fjc/fis/view/vendor/VendorListView.java
+++ src/main/java/gov/fjc/fis/view/vendor/VendorListView.java
@@ -1,10 +1,16 @@
 package gov.fjc.fis.view.vendor;
 
+import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.data.renderer.Renderer;
 import com.vaadin.flow.router.Route;
 import gov.fjc.fis.entity.Vendor;
 import gov.fjc.fis.view.main.MainView;
+import io.jmix.flowui.UiComponents;
 import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.awt.*;
 
 
 @Route(value = "vendors", layout = MainView.class)
@@ -13,5 +19,17 @@ import io.jmix.flowui.view.*;
 @LookupComponent("vendorsDataGrid")
 @DialogMode(width = "64em")
 public class VendorListView extends StandardListView<Vendor> {
+    @Autowired
+    private UiComponents uiComponents;
+
+    @Supply(to = "vendorsDataGrid.active", subject = "renderer")
+    protected Renderer<Vendor> vendorsDataGridActiveRenderer() {
+        return new ComponentRenderer<>(vendor -> {
+            Checkbox checkbox = uiComponents.create(Checkbox.class);
+            checkbox.setValue(vendor.getActive());
+            checkbox.setReadOnly(true);
+            return checkbox;
+        });
+    }
 
 }
