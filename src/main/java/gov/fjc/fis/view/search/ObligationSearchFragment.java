9b55fcbe0294a751490a3a0a81480f6048531389
Doug Mitchell <dmitchell@fjc.gov>
2025-03-26 13:39:23 -0400

    obligation search fragment propertyfilter restoration

--- src/main/java/gov/fjc/fis/view/search/ObligationSearchFragment.java
+++ src/main/java/gov/fjc/fis/view/search/ObligationSearchFragment.java
@@ -9,44 +9,101 @@ import io.jmix.flowui.component.select.JmixSelect;
 import io.jmix.flowui.fragment.FragmentDescriptor;
 import io.jmix.flowui.view.*;
 
+import java.util.Map;
+
 @FragmentDescriptor("obligation-search-fragment.xml")
 public class ObligationSearchFragment extends EntitySearchFragment {
-    @ViewComponent
-    private PropertyFilter<Boolean> statusSearch;
+
     @ViewComponent
     private HorizontalLayout mocBox;
     @ViewComponent
     private HorizontalLayout bocBox;
-
-//    private EntityComboBox<Category> categorySearchField;
-//    private EntityComboBox<ObjectClass> objectClassSearchField;
+    @ViewComponent
+    private PropertyFilter<Object> docnumSearch;
+    @ViewComponent
+    private PropertyFilter<Object> vendorSearch;
+    @ViewComponent
+    private PropertyFilter<Object> actnumSearch;
+    @ViewComponent
+    private PropertyFilter<Object> amountSearch;
+    @ViewComponent
+    private PropertyFilter<Boolean> statusSearch;
+    @ViewComponent
+    private PropertyFilter<Object> bpoSearch;
+    @ViewComponent
+    private PropertyFilter<Object> programStartSearch;
+    @ViewComponent
+    private PropertyFilter<Object> programEndSearch;
+    @ViewComponent
+    private PropertyFilter<Object> travelStartSearch;
+    @ViewComponent
+    private PropertyFilter<Object> travelEndSearch;
 
     @Override
     protected void additionalFragmentActions() {
-        ((JmixSelect<Boolean>) statusSearch.getValueComponent()).setItemLabelGenerator(status -> {
-            return status == null ? "" : status ? "Open" : "Closed";
-//            if (status == null) {
-//                return "";
-//            }
-//            return status ? "Open" : "Closed";
-        });
+        ((JmixSelect<Boolean>) statusSearch.getValueComponent())
+                .setItemLabelGenerator(status -> status == null ? "" : status ? "Open" : "Closed");
+    }
+
+    @Override
+    public void setPropertyFilters(Map<String, Object> filters) {
+        for (var entry : filters.entrySet()) {
+            var key = entry.getKey();
+            var value = entry.getValue();
+            switch (key) {
+                case "documentNumber":
+                    docnumSearch.setValue(value);
+                    break;
+                case "vendor":
+                    vendorSearch.setValue(value);
+                    break;
+                case "activity.activityNumber":
+                    actnumSearch.setValue(value);
+                    break;
+                case "amount":
+                    amountSearch.setValue(value);
+                    break;
+                case "amount_op":
+                    amountSearch.setOperation((PropertyFilter.Operation) value);
+                    break;
+                case "status":
+                    statusSearch.setValue((Boolean) value);
+                    break;
+                case "blanketPurchaseOrder":
+                    bpoSearch.setValue(value);
+                    break;
+                case "activity.startDate":
+                    programStartSearch.setValue(value);
+                    break;
+                case "activity.startDate_op":
+                    programStartSearch.setOperation((PropertyFilter.Operation) value);
+                    break;
+                case "activity.endDate":
+                    programEndSearch.setValue(value);
+                    break;
+                case "activity.endDate_op":
+                    programEndSearch.setOperation((PropertyFilter.Operation) value);
+                    break;
+                case "travelStartDate":
+                    travelStartSearch.setValue(value);
+                    break;
+                case "travelStartDate_op":
+                    travelStartSearch.setOperation((PropertyFilter.Operation) value);
+                    break;
+                case "travelEndDate":
+                    travelEndSearch.setValue(value);
+                    break;
+                case "travelEndDate_op":
+                    travelEndSearch.setOperation((PropertyFilter.Operation) value);
+                    break;
+            }
+        }
     }
 
     @Override
     public void addCategoryObjectClass(EntityComboBox<Category> categorySearchField,
                                        EntityComboBox<ObjectClass> objectClassSearchField) {
-//        categorySearchField = category;
-//        mocBox.add(categorySearchField);
-//        objectClassSearchField = objectClass;
-//        bocBox.add(objectClassSearchField);
         mocBox.add(categorySearchField);
         bocBox.add(objectClassSearchField);
     }
-
-//    @Override
-//    public void clearPropertyFilters() {
-//        super.clearPropertyFilters();
-//        categorySearchField.setValue(null);
-//        objectClassSearchField.setValue(null);
-//    }
 }
