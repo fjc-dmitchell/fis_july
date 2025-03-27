da8e265332f66f9f57a619e6e33022aa3dd5f233
Doug Mitchell <dmitchell@fjc.gov>
2025-03-27 12:09:29 -0400

    obligation list view changes for search

--- src/main/java/gov/fjc/fis/view/obligation/ObligationListView.java
+++ src/main/java/gov/fjc/fis/view/obligation/ObligationListView.java
@@ -1,12 +1,18 @@
 package gov.fjc.fis.view.obligation;
 
+import com.vaadin.flow.data.selection.SelectionEvent;
 import gov.fjc.fis.entity.Obligation;
 
+import gov.fjc.fis.event.SearchGridSelectedItemsEvent;
 import gov.fjc.fis.view.main.MainView;
 
 import com.vaadin.flow.router.Route;
 import gov.fjc.fis.view.search.CustomSearchFragment;
+import io.jmix.core.session.SessionData;
+import io.jmix.flowui.UiEventPublisher;
+import io.jmix.flowui.component.grid.DataGrid;
 import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Route(value = "obligations", layout = MainView.class)
 @ViewController("fis_Obligation.list")
@@ -14,8 +20,14 @@ import io.jmix.flowui.view.*;
 @LookupComponent("obligationsDataGrid")
 @DialogMode(width = "64em")
 public class ObligationListView extends StandardListView<Obligation> {
+    @Autowired
+    private UiEventPublisher uiEventPublisher;
+    @Autowired
+    private SessionData sessionData;
     @ViewComponent
     private CustomSearchFragment searchFragment;
+    @ViewComponent
+    private DataGrid<Obligation> obligationsDataGrid;
 
     private boolean fjcFoundation = false;
 
@@ -26,5 +38,12 @@ public class ObligationListView extends StandardListView<Obligation> {
     @Subscribe
     protected void onBeforeShow(final BeforeShowEvent event) {
         searchFragment.setFjcFoundation(fjcFoundation);
+        searchFragment.setDataGrid(obligationsDataGrid);
+    }
+
+    @Subscribe("obligationsDataGrid")
+    protected void onObligationsDataGridSelection(final SelectionEvent<DataGrid<Obligation>, Obligation> event) {
+        sessionData.setAttribute("searchDataGridSize", event.getAllSelectedItems().size());
+        uiEventPublisher.publishEvent(new SearchGridSelectedItemsEvent(this, "searchGridChanged"));
     }
 }
