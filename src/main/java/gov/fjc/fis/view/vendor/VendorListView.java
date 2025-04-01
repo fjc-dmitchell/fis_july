083ad0f21c38081288603280df0bbbe24b17ef1d
Doug Mitchell <dmitchell@fjc.gov>
2025-04-01 18:52:47 -0400

    list view refinements

--- src/main/java/gov/fjc/fis/view/vendor/VendorListView.java
+++ src/main/java/gov/fjc/fis/view/vendor/VendorListView.java
@@ -1,12 +1,18 @@
 package gov.fjc.fis.view.vendor;
 
+import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.data.renderer.Renderer;
 import com.vaadin.flow.router.Route;
 import gov.fjc.fis.entity.Vendor;
 import gov.fjc.fis.view.main.MainView;
 import io.jmix.flowui.UiComponents;
+import io.jmix.flowui.component.SupportsLabelPosition;
+import io.jmix.flowui.component.UiComponentUtils;
+import io.jmix.flowui.component.propertyfilter.PropertyFilter;
+import io.jmix.flowui.kit.component.button.JmixButton;
 import io.jmix.flowui.view.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -21,6 +27,33 @@ import java.awt.*;
 public class VendorListView extends StandardListView<Vendor> {
     @Autowired
     private UiComponents uiComponents;
+    @ViewComponent
+    private HorizontalLayout searchBox;
+
+    @Subscribe
+    protected void onBeforeShow(final BeforeShowEvent event) {
+        for (var component : UiComponentUtils.getComponents(searchBox)) {
+            if (component instanceof PropertyFilter<?>) {
+                setFilterProperties((PropertyFilter<?>) component);
+            }
+        }
+    }
+
+    private void setFilterProperties(PropertyFilter<?> filter) {
+        filter.setAutoApply(true);
+        filter.setLabelPosition(SupportsLabelPosition.LabelPosition.TOP);
+    }
+
+    @Subscribe(id = "clearSearchBtn", subject = "clickListener")
+    protected void onClearSearchBtnClick(final ClickEvent<JmixButton> event) {
+        for (var component : UiComponentUtils.getComponents(searchBox)) {
+            if (component instanceof PropertyFilter<?>) {
+                ((PropertyFilter<?>) component).clear();
+                ((PropertyFilter<?>) component).getQueryCondition().setParameterValue(null);
+                ((PropertyFilter<?>) component).apply();
+            }
+        }
+    }
 
     @Supply(to = "vendorsDataGrid.active", subject = "renderer")
     protected Renderer<Vendor> vendorsDataGridActiveRenderer() {
@@ -32,5 +65,4 @@ public class VendorListView extends StandardListView<Vendor> {
             return checkbox;
         });
     }
-
 }
