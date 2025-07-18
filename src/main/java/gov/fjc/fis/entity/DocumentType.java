a3b37b6805f4020c5fb9cabc1390b9ebbc6e9ba1
Doug Mitchell <dmitchell@fjc.gov>
2025-07-18 14:11:22 -0400

    DocumentType enumeration type for MO and TA

---
+++ src/main/java/gov/fjc/fis/entity/DocumentType.java
@@ -0,0 +1,32 @@
+package gov.fjc.fis.entity;
+
+import io.jmix.core.metamodel.datatype.EnumClass;
+
+import org.springframework.lang.Nullable;
+
+
+public enum DocumentType implements EnumClass<String> {
+
+    MISCELLANEOUS_OBLIGATION("MOJ"),
+    TRAVEL_AUTHORIZATION("TAJ");
+
+    private final String id;
+
+    DocumentType(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Nullable
+    public static DocumentType fromId(String id) {
+        for (DocumentType at : DocumentType.values()) {
+            if (at.getId().equals(id)) {
+                return at;
+            }
+        }
+        return null;
+    }
+}
