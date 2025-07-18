e1d47123ca1123798c43973aa3584f16d9cf9832
Doug Mitchell <dmitchell@fjc.gov>
2025-07-18 13:05:36 -0400

    FCN and Invoice dialog views for obligation detail

---
+++ src/main/java/gov/fjc/fis/view/fundcontrolnotice/FundControlNoticeDetailDialogView.java
@@ -0,0 +1,48 @@
+package gov.fjc.fis.view.fundcontrolnotice;
+
+import com.vaadin.flow.component.AbstractField;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.router.Route;
+import gov.fjc.fis.entity.FundControlNotice;
+import gov.fjc.fis.view.fileattachmentfragment.FileAttachmentFragment;
+import gov.fjc.fis.view.main.MainView;
+import io.jmix.core.EntityStates;
+import io.jmix.flowui.component.datepicker.TypedDatePicker;
+import io.jmix.flowui.component.textarea.JmixTextArea;
+import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDate;
+
+@Route(value = "fund-control-notices-dialog/:id", layout = MainView.class)
+@ViewController(id = "fis_FundControlNotice_dialog.detail")
+@ViewDescriptor(path = "fund-control-notice-detail-dialog-view.xml")
+@EditedEntityContainer("fundControlNoticeDc")
+public class FundControlNoticeDetailDialogView extends StandardDetailView<FundControlNotice> {
+    @Autowired
+    private EntityStates entityStates;
+    @ViewComponent
+    private FileAttachmentFragment attachmentFragment;
+    @ViewComponent
+    private TypedDatePicker<Comparable> fcnDateField;
+    @ViewComponent
+    private Paragraph createdByString;
+    @ViewComponent
+    private JmixTextArea memoField;
+
+    @Subscribe
+    protected void onBeforeShow(final BeforeShowEvent event) {
+        var fcn = getEditedEntity();
+        attachmentFragment.setHostEntity(fcn);
+        if (entityStates.isNew(fcn)) {
+            fcnDateField.setValue(LocalDate.now());
+        } else {
+            createdByString.setText(fcn.getCreatedByString());
+        }
+    }
+
+    @Subscribe("memoField")
+    protected void onMemoFieldComponentValueChange(final AbstractField.ComponentValueChangeEvent<JmixTextArea, ?> event) {
+        memoField.setValue(((String) event.getValue()).trim());
+    }
+}
---
+++ src/main/java/gov/fjc/fis/view/invoice/InvoiceDetailDialogView.java
@@ -0,0 +1,52 @@
+package gov.fjc.fis.view.invoice;
+
+import com.vaadin.flow.component.AbstractField;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.router.Route;
+import gov.fjc.fis.entity.Invoice;
+import gov.fjc.fis.view.fileattachmentfragment.FileAttachmentFragment;
+import gov.fjc.fis.view.main.MainView;
+import io.jmix.core.EntityStates;
+import io.jmix.flowui.component.datepicker.TypedDatePicker;
+import io.jmix.flowui.component.textarea.JmixTextArea;
+import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Route(value = "invoices-dialog/:id", layout = MainView.class)
+@ViewController(id = "fis_Invoice_dialog.detail")
+@ViewDescriptor(path = "invoice-detail-dialog-view.xml")
+@EditedEntityContainer("invoiceDc")
+public class InvoiceDetailDialogView extends StandardDetailView<Invoice> {
+    @Autowired
+    private EntityStates entityStates;
+    @ViewComponent
+    private FileAttachmentFragment attachmentFragment;
+    @ViewComponent
+    private TypedDatePicker<Date> invoiceDateField;
+    @ViewComponent
+    private TypedDatePicker<Date> paymentDateField;
+    @ViewComponent
+    private Paragraph createdByString;
+    @ViewComponent
+    private JmixTextArea memoField;
+
+    @Subscribe
+    protected void onBeforeShow(final BeforeShowEvent event) {
+        var invoice = getEditedEntity();
+        attachmentFragment.setHostEntity(invoice);
+        if (entityStates.isNew(invoice)) {
+            invoiceDateField.setValue(LocalDate.now());
+            paymentDateField.setValue(LocalDate.now());
+        } else {
+            createdByString.setText(invoice.getCreatedByString());
+        }
+    }
+
+    @Subscribe("memoField")
+    protected void onMemoFieldComponentValueChange(final AbstractField.ComponentValueChangeEvent<JmixTextArea, ?> event) {
+        memoField.setValue(((String) event.getValue()).trim());
+    }
+}
